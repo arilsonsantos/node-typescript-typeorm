f60957b177d7da98a8c2b6cacbefd98048e62e3a
arilsonsantos <arilsosantos@gmail.com>
2024-10-21 17:55:03 -0300

    Role repository findbyName

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,9 +1,13 @@
 import {
+    name,
     Router
 } from "express"
 import {
     RolesRepository
 } from "@roles/repositories/RolesRepository";
+import {
+    AppError
+} from "@shared/errors/AppError";
 
 const rolesRouter = Router()
 const rolesRepository = new RolesRepository()
@@ -11,6 +15,10 @@ const rolesRepository = new RolesRepository()
 rolesRouter.post("/", (reqquest, response) => {
     const  { name }  = reqquest.body
 
+    const roleExists = rolesRepository.findByName(name)
+    if (roleExists){
+        throw new AppError('Role already exists')
+    }
     const role = rolesRepository.create( { name })
 
     return response.status(201).json(role)
