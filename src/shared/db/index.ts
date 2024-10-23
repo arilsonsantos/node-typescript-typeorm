60eab7c31090999d6661081539bf99b51dc35680
arilsonsantos <arilsosantos@gmail.com>
2024-10-23 20:13:41 -0300

    Migrations UsersTable
    npm run typeorm migration:create src/shared/db/migrations/CreateUsersTable
    
    npm run typeorm -- -d src/shared/db/index.ts migration:run

--- src/shared/db/index.ts
+++ src/shared/db/index.ts
@@ -5,12 +5,16 @@ import {
 import {
     Role
 } from "@roles/entities/Roles"
+import {
+    CreateUsersTable1729724909949
+} from "@shared/db/migrations/1729724909949-CreateUsersTable"
 
 export const dataSource = new DataSource({
     type: 'sqlite',
     database: './db.sqlite',
     entities: [Role],
     migrations: [
-        CreateRolesTable1729598710749
+        CreateRolesTable1729598710749,
+        CreateUsersTable1729724909949
     ]
 })
